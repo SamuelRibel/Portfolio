35d706fb9e7a40001264df98a83111e9b3ff30ed
Samuel <samuelribeirocontato@gmail.com>
2025-07-17 15:45:12 -0300

    adicionando png de bandeira

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { SideBar } from "./components/SideBar"
-// import { Hero } from "./components/Hero"
+import { Hero } from "./components/Hero"
 // import { About } from "./components/About"
 // import { Projects } from "./components/Projects"
 // import { Contact } from "./components/contacts"
@@ -11,6 +11,7 @@ export default function App() {
       <main>
         {/* <SideBar /> */}
         <Header />
+        <Hero />
         {/* <Hero />
         <About />
         <Projects />
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import Bandeira from "../assets/Img/Bandeira"
+
+export function Hero(){
+    return(
+        <div className="hero-block">
+            <h1 className="hero-name">Samuel Ribeiro</h1>
+            <p className="under-name">Minas Gerais, BR | Full-Stack developer</p>
+            <p className="phrase">"I am always doing that which I cannot do, in order that I may learn how to do it." — Pablo Picasso</p>
+            <div className="">
+
+            </div>
+        </div>
+        
+        
+    )
+}
