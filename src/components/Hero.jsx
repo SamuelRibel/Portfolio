ddb721ba970e226f58012ba78f8d09d9ef8fa0de
Samuel <samuelribeirocontato@gmail.com>
2025-07-21 12:36:16 -0300

    adicionando e configurando demonstrativos de tecnologias

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,9 +9,9 @@ export function Hero(){
                 <p className="under-name">Minas Gerais, BR | Full-Stack developer</p>
             </div>
             <div className="div-button">
-                <a href="https://www.linkedin.com/in/sam-ribel/" target="_blank"><button className="button">LINKEDIN</button></a>
-                <a href="https://github.com/SamuelRibel" target="_blank"><button className="button">GITHUB</button></a>
-                <a href="/Samuels_CV.pdf" target="_blank"><button className="button">RESUME</button></a>
+                <a href="https://www.linkedin.com/in/sam-ribel/" target="_blank"><button className="links">LINKEDIN</button></a>
+                <a href="https://github.com/SamuelRibel" target="_blank"><button className="links">GITHUB</button></a>
+                <a href="/Samuels_CV.pdf" target="_blank"><button className="links">RESUME</button></a>
             </div>
             <p className="phrase">"I am always doing that which I cannot do, in order that I may learn how to do it." — Pablo Picasso</p>
             <div className="">
--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,6 +1,35 @@
+import { FaReact } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3 } from "react-icons/fa";
+
 export function Stack(){
     return(
-        <div>
+        
+        <div className="container">
+            
+            <div className="content-box">
+                <h3 className="stack-name">Front-End</h3>
+                <div className="stack-front">
+                    <div className="tech-front">
+                        <span ><FaHtml5 /></span>
+                        <span>HTML</span>
+                    </div>
+                    <div className="tech-front">
+                        <span ><FaCss3 /></span>
+                        <span>CSS</span>
+                    </div>
+                    <div className="tech-front">
+                        <span ><FaReact /></span>
+                        <span>React</span>
+                    </div> 
+                </div>
+            </div>
+            <div className="content-box">
+                <h3 className="stack-name">Back-end</h3>
+            </div>
+            <div className="content-box">
+                <h3 className="stack-name">Others</h3>
+            </div>
             
         </div>
     )
